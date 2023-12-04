4daf45268a933002ff6b4155235f95e9cc32514a
bharath063 <5726047+bharath063@users.noreply.github.com>
2023-12-04 14:24:38 +0530

    try Fix WebexHelper import

--- webex_list_meetings.py
+++ webex_list_meetings.py
@@ -1,11 +1,36 @@
-import requests
-import json
-
 from typing import Type, Optional
 from pydantic import BaseModel, Field
 from superagi.tools.base_tool import BaseTool
-from helper.webex_helper import WebexHelper
+# from helper.webex_helper import WebexHelper
 from wxc_sdk import WebexSimpleApi
+
+
+import tiktoken
+
+class WebexHelper:
+    def __init__(self, webex_token):
+        """
+        Initializes the WebexHelper with the provided webex token.
+
+        Args:
+            Webex_token (str): Personal Webex token.
+        """
+        self.webex_token = webex_token
+    
+    @staticmethod
+    def count_text_tokens(message: str) -> int:
+        """
+        Function to count the number of tokens in a text.
+
+        Args:
+            message (str): The text to count the tokens for.
+
+        Returns:
+            int: The number of tokens in the text.
+        """
+        encoding = tiktoken.get_encoding("cl100k_base")
+        num_tokens = len(encoding.encode(message)) + 4
+        return num_tokens
 
 
 class WebexFetchMeetingsSchema(BaseModel):
